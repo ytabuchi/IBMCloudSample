357a55c8a20d98e2e62fa5f1fe188e0d09ddb745
chomado <chomado@gmail.com>
2018-06-10 00:30:45 +0900

    Azure 呼び出し処理の実装

--- IBMCloudSample/Azure.NetStandardCore/CognitiveClient.cs
+++ IBMCloudSample/Azure.NetStandardCore/CognitiveClient.cs
@@ -1,14 +1,42 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using CloudServices.Core;
+using Newtonsoft.Json;
 
 namespace Azure.NetStandardCore
 {
     public class CognitiveClient : ICloudService
     {
-        public Task<string> GetResponseAsync(string input)
+        public async Task<string> GetResponseAsync(string input)
         {
-            return Task.FromResult($"echo+ {input}");
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(
+                    JsonConvert.SerializeObject(new { text = input }),
+                    Encoding.UTF8,
+                    "application/json");
+                var response = await client.PostAsync(Secrets.Endpoint, content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return "エラーが発生しました。";
+                }
+
+                var result = JsonConvert.DeserializeObject<CognitiveResult>(await response.Content.ReadAsStringAsync());
+                if (!string.IsNullOrEmpty(result.Message))
+                {
+                    return result.Message;
+                }
+
+                var sentimentAverage = (int)(result.Sentiments.Average() * 100);
+                return $@"{result.TwitterName}さんは{sentimentAverage}%ポジティブです☺
+
+直近 {result.Sentiments.Length} 件のツイートのキーワードは 
+{string.Join(", ", result.Keyphreases)}
+です。";
+            }
         }
     }
 }
---
+++ IBMCloudSample/Azure.NetStandardCore/CognitiveResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace Azure.NetStandardCore
+{
+    public class CognitiveResult
+    {
+        [JsonProperty("twitterName")]
+        public string TwitterName { get; set; }
+        [JsonProperty("keyphreases")]
+        public string[] Keyphreases { get; set; }
+        [JsonProperty("sentiments")]
+        public double[] Sentiments { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
