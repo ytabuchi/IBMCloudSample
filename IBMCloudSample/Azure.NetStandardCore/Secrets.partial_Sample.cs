62c82d150e7519d61c0e654a74ef0455701a4564
Yoshito Tabuchi @ MBP-Xamarin <ytabuchi.xlsoft@gmail.com>
2018-06-11 12:20:35 +0900

    ignore secrets.partial.cs

--- IBMCloudSample/Azure.NetStandardCore/Secrets.partial_Sample.cs
+++ IBMCloudSample/Azure.NetStandardCore/Secrets.partial_Sample.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 namespace Azure.NetStandardCore
 {
-    public static partial class Secrets
-    {
-        public static readonly string Endpoint = "https://prod-19.japanwest.logic.azure.com:443/workflows/f6855fe5a99940ff971e1d949ee9bc7b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=jZKxZuRnDOdxqzgZGbxvVru_FxvlJJQtqWgoMINPVHY";
-    }
+    //public static partial class Secrets
+    //{
+    //    public static readonly string Endpoint = "Logic Apps Endpoint URL";
+    //}
 }
